2ebcb4c1c989c3d558b2ea2de2607b5ce5cb25c0
vra <wyf.brz@gmail.com>
2018-11-22 00:45:41 +0800

    add setup.py, move all functions to __init__.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+""" Setup scripts for reuse. """
+from setuptools import setup, find_packages
+
+
+setup(
+    name="py-reuse",
+    version="0.0.1",
+    description="Collection of useful python functions",
+    url="https://github.com/vra/reuse",
+    author="Yunfeng Wang",
+    author_email="wyf.brz@gmail.com",
+    packages=find_packages()
+)
